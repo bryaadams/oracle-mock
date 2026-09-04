3a0f31bec53774b8490ed3f36126ce81cc131957
bryaadams <bryaadams@deloitte.com>
2026-09-04 18:15:39 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const url = require('url');
+
+const people = [
+    {
+        person_id: "1004",
+        first_name: "test",
+        last_name: "4",
+        email: "test4@test.com",
+        active: 1
+    }
+]
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    res.setHeader(
+        "Content-Type",
+        "application/json"
+    );
+
+    if (req.method === "GET" && parsedUrl.pathname === "/ords/hr/people") {
+        res.writeHead(200);
+        res.end(JSON.stringify({
+            items: people
+        }));
+        return;
+    }
+
+    res.writeHead(404);
+    res.end(JSON.stringify({
+        error: "Not Found"
+    }))
+})
+
+const PORT = 8181;
+
+server.listen(PORT, () => {
+    console.log("Mock oracle API running on http://localhost:8181")
+})
